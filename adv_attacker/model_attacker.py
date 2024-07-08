2e6c5db2b9252bdd56b0847227f944a5ddd9640e
matttrd <matteoterzi.mt@gmail.com>
2024-07-08 21:56:23 +0100

    added minimal code for attacker

--- adv_attacker/model_attacker.py
+++ adv_attacker/model_attacker.py
@@ -1,1 +1,88 @@
 
+from typing import Optional
+from torch.nn import Module
+from torch import Tensor
+import torch
+from adv_attacker.attacks import L2Attack, LinfAttack
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class ModelAttacker(Module):
+    # TODO : docstring
+    def __init__(self, model, attack_norm: str, loss_function: Optional[callable] = None):
+        super(ModelAttacker, self).__init__()
+        self._model = model
+        if attack_norm == 'l2':
+            self.attack_class = L2Attack
+        elif attack_norm == 'linf':
+            self.attack_class = LinfAttack
+        else:
+            raise ValueError(f"The attacker {attack_norm} is not implemented.")
+        
+        # TODO: we may want custom losses
+        if loss_function is None:
+            # use standard one
+            self._loss_function = torch.nn.CrossEntropyLoss(reduction='none').to(device=device)
+
+    def _get_loss_and_output(self, input: Tensor, target: Tensor) -> tuple[Tensor, Tensor]:
+        """
+        Compute the loss for the batch.
+        
+        :param input: The input image tensor.
+        :param target: the target class tensor.
+        :return: A tuple of output and loss for the input image.
+        """
+        output = self._model(input)
+        return self._loss_function(output, target), output
+
+    def forward(self,
+            input: Tensor,
+            target_class: Tensor, # TODO: make this optional to allow to apply non-targeted attacks
+            eps: Optional[float] = None,
+            attack_rate: float = 0.01,
+            num_steps: int = 8,
+            skip_adv_attack: bool = False,
+            ) -> tuple[Tensor, Tensor]:
+        
+        if not skip_adv_attack:
+            # TODO: implement forward method for attacker
+            assert eps is not None, "`eps` must be a valid float number"
+            input = input.to(device=device)
+            assert target_class is not None, "Must specify a target class in the current implementation."
+                        
+            attacker = self._attack_class(original_input=input, eps=eps, attack_rate=attack_rate)
+            adv_image = input.detach()
+            
+            # TODO: run the attack. Steps:
+                # 1. put the model in eval (avoid to update batchnorm stats)
+                # 2. Attack
+                # 3. Return the image
+            
+            self._model.eval()
+
+            # TODO: add change of random noise before starting to avoid bad gradients
+            # TODO: add multiple trials if get the best example
+            # TODO: choose the best instead of the last one
+
+            # iterate for num_steps
+            for _ in range(num_steps):
+                # we need to copy the image and detach to avoid the gradient propagation
+                adv_image = adv_image.clone().detach()
+                # we need to compute the grad so we must activate it
+                adv_image.requires_grad_(True)
+                batch_loss, batch_output = self._get_loss_and_output(input=adv_image, target=target_class)
+                loss = batch_loss.mean()
+                # TODO: modify here if want to do non-target attacks
+                grad, = torch.autograd.grad(loss, [adv_image])
+
+                # now we need to apply the attack but make sure we don't compute any gradient (we may not need this)
+                with torch.no_grad():
+                    attacker.apply_attack_iteration(input=adv_image, grad=grad)
+                    # project back to make sure to stay in the correct interval
+                    adv_image = attacker.project_back(adv_image)
+
+        else:
+            adv_image = input
+
+        output = self._model(adv_image)
+        return output, input
--- tests/test_adversarial.py
+++ tests/test_adversarial.py
@@ -1,5 +1,17 @@
 import unittest
 import numpy
+from PIL import Image
+from adv_attacker import ModelAttacker
+import torch
+
+from torchvision.models import resnet18, ResNet18_Weights # import a small model
+weights = ResNet18_Weights.DEFAULT
+model = resnet18(weights=weights)
+preprocess = weights.transforms()
+
+# determine the device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
 
 class TestAdversarialAttack(unittest.TestCase):
     @classmethod
@@ -7,15 +19,27 @@ class TestAdversarialAttack(unittest.TestCase):
         # TODO: initialization
         #
         cls.rng = numpy.random.default_rng()
+        cls.l2_attacker = ModelAttacker(model=model, attack_norm='l2')
+        # load a test image
+        image_path = 'tests/images/dog_image.jpeg'  # Replace with your image path
+        img = Image.open(image_path)
+        img_torch = preprocess(img)
+        # denormalize image
+        # TODO: add normalization inside the core functions to avoid this step
+        cls.img_torch = img_torch * torch.tensor(preprocess.std)[:, None, None] + torch.tensor(preprocess.mean)[:, None, None]
+        cls.img_torch =  cls.img_torch.to_device(device=device)
 
     def test_api(self):
-        # TODO: add this
-        pass
+        # unsqueeze image
+        # TODO: improve the code to avoid this step
+        img = self.img_torch.unsqueeze(0)
+        # test it returns the same image
+        output, input = self.l2_attacker(input=img, skip_adv_attack=True)
+        # must be the same 
+        torch.testing.assert_close(input, img)
+
 
     def test_single_image(self):
-        # TODO: add this
-        pass
-
-    def test_multiple_image(self):
-        # TODO: add this
-        pass
+        
+        img = self.img_torch.unsqueeze(0)
+        output, input = self.l2_attacker()
